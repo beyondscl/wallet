4ee8356f0d9cf3945c34b39dddd217f9275597e0
liming.liu <liming.liu@71bird.com>
2018-08-13 14:47:31 +0800

    fix: 修改对交易gasPrice的限制提升

--- myLaya2/src/view/SmartCat.ts
+++ myLaya2/src/view/SmartCat.ts
@@ -29,15 +29,34 @@ module view {
             this.comp.inviter_earning.on(Laya.Event.CLICK, this, this.goTo, [6]);
             this.comp.apply_vip.on(Laya.Event.CLICK, this, this.goTo, [7]);
             this.comp.smartcat_explain.on(Laya.Event.CLICK, this, this.goTo, [8]);
+
+            this.comp.btn_assets.on(Laya.Event.CLICK, this, this.goTo, [10]);
+            this.comp.btn_smartcat.on(Laya.Event.CLICK, this, this.goTo, [11]);
+            this.comp.btn_me.on(Laya.Event.CLICK, this, this.goTo, [12])
         }
         
         private goTo(index: number): void {
             switch (index) {
                 case 1:
                     this.comp.visible = false;
-                    let wm = new view.smartcat.MySmartcat()
-                    wm.setParentUI(this);
+                    let sms = new view.smartcat.MySmartcat()
+                    sms.setParentUI(this);
                     break;
+                case 10: // 前往资产页面
+                    this.comp.visible = false;
+                    this.parentUI.comp.visible = true;
+                    break;
+                case 11: //前往智能猫页面，当前页面
+                    break;
+                case 12:
+                    this.comp.visible = false;
+                    if (util.getMeView()) {
+                        util.getMeView().comp.visible = true;
+                    } else {
+                        new view.WalletMe();
+                    }
+                    // this.parentUI.comp.visible = true;
+                    break; //前往个人页面
                 default:
                     break;
             }
